3dae86d708405f51be32275463a50852f7e0b3e4
C. SHEN <chunhua@me.com>
2024-10-12 13:56:32 +1030

    CS: commit save

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -37,6 +37,8 @@ require('lazy').setup({
   require 'kickstart/plugins/comment',
   require 'kickstart/plugins/gruvbox', -- color scheme
 
+  require 'kickstart/plugins/noice',
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
