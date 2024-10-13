c2fe167878d8d8381254eb7bb5002d21ce8ec0b5
C. SHEN <chunhua@me.com>
2024-10-13 15:11:08 +1030

    CS: commit save

--- init.lua
+++ init.lua
@@ -58,13 +58,7 @@ Kickstart Guide:
     Throughout the file. These are for you, the reader, to help you understand what is happening.
     Feel free to delete them once you know what you're doing, but they should serve as a guide
     for when you are first encountering a few different constructs in your Neovim config.
-
 If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
 -- Set <space> as the leader key
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -38,6 +38,7 @@ require('lazy').setup({
   require 'kickstart/plugins/gruvbox', -- color scheme
 
   require 'kickstart/plugins/noice',
+  require 'kickstart/plugins/modicator', -- change current line color according to mode
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -52,7 +53,7 @@ require('lazy').setup({
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
