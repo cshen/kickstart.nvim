5a0c7c18aaca52a7446b44c60ea1b212712c42c7
C. SHEN <chunhua@me.com>
2024-10-17 00:45:35 +1030

    CS: commit save

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -39,7 +39,7 @@ require('lazy').setup({
 
   require 'kickstart/plugins/noice',
   require 'kickstart/plugins/modicator', -- change current line color according to mode
-
+  require 'kickstart/plugins/barbar', -- Tab manager
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
